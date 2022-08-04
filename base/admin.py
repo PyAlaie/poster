29cbc524d32c78076b216c549e4b7d0f065f2ee0
Arshia Alaie <arshiaalaie1@gmail.com>
2022-08-04 09:29:24 +0430

    gemeral minor changes
    added static files urls
    made minor changes in templates
    added new model to admin panel

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from base.models import post,tag,comment
+from base.models import post, profile,tag,comment
 
 
 # Register your models here.
 admin.site.register(post)
 admin.site.register(tag)
+admin.site.register(profile)
