2231dfcc548ce0aea67a528168112b59ddfa777d
Arshia Alaie <arshiaalaie1@gmail.com>
2022-08-04 09:04:09 +0430

    added media and static files settings

--- poster/settings.py
+++ poster/settings.py
@@ -120,7 +120,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
--- poster/urls.py
+++ poster/urls.py
@@ -1,8 +1,13 @@
+import imp
 from xml.etree.ElementInclude import include
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path,include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
