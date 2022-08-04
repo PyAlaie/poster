49eaa90bbde232ca401a3bf3784efbae6d08b329
Arshia Alaie <arshiaalaie1@gmail.com>
2022-08-04 09:26:53 +0430

    added profile model

--- base/models.py
+++ base/models.py
@@ -1,5 +1,7 @@
+from distutils.command.upload import upload
 from django.db import models
 from django.contrib.auth.models import User
+from django_countries.fields import CountryField
 
 # Create your models here.
 class tag(models.Model):
@@ -30,3 +32,21 @@ class comment(models.Model):
     
     def __str__(self):
         return self.text[0:20]
+    
+class profile(models.Model):
+    genders = [
+        ('N', 'Rather not to say'),
+        ('M', 'Male'),
+        ('F', 'Female'),
+    ]
+    
+    avatar = models.ImageField(null=True, blank=True, upload_to='avatars')
+    full_name = models.CharField(max_length=100,null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    gender = models.CharField(max_length=1, default='N', choices=genders)
+    country = CountryField(null=True, blank=True)
+    related_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birth_date = models.DateField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.related_user
